69469302f67ed595836f8aee2d0d5403b70f64e4
Alan Nascimento <alannascioment@gmail.com>
2023-03-29 23:38:16 -0300

    Remoção do botão com icone de email

--- app/src/main/java/com/example/magister/MainActivity.kt
+++ app/src/main/java/com/example/magister/MainActivity.kt
@@ -26,11 +26,11 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
+        //val fab: FloatingActionButton = binding.fab
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        //fab.setOnClickListener { view ->
+        //    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //        .setAction("Action", null).show()
+        //}
     }
 }
