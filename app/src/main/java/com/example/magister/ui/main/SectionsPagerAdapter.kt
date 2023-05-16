79b42ef3f19fadb7ec8caa06dd584ed9dce3b03a
Emerson Rodrigues da Costa <70579020+Emerson0307@users.noreply.github.com>
2023-05-16 04:10:42 -0300

    Parte de login e cadastro feitas, falta linkar as Tabs com páginas.

--- app/src/main/java/com/example/magister/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/magister/ui/main/SectionsPagerAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.magister.ui.main
 
 import android.content.Context
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -8,7 +9,8 @@ import com.example.magister.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
@@ -30,6 +32,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
 
     override fun getCount(): Int {
         // Show 2 total pages.
-        return 2
-    }
-}
+        return 3
+    }}
+
+
